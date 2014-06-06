8d7c48374aba05db1b88359a5e74dbf96e629ce2
stellbe <remydagorne@hotmail.fr>
2014-06-06 17:21:17 +0200

    - Remplissage du textField par les boutons - suppression du textFiled par le bouton btClear + Images Notes

--- src/vues/NewPartition.java
+++ src/vues/NewPartition.java
@@ -12,141 +12,243 @@ import java.awt.GridBagLayout;
 import javax.swing.BorderFactory;
 import javax.swing.GroupLayout;
 import javax.swing.JButton;
+import javax.swing.JComponent;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.DropMode;
 
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class NewPartition extends JPanel {
 	
-	private JTextField tfNotes;
+	private JTextField tfNotes, tfName;
 	private JButton bDo, bRe, bMi, bFa, bSo, bLa, bti, bDo1, bRe1,
-					bMi1, bFa1, bSo1, bLa1, bti1, bDo2;
-	private JLabel label;
-	JPanel panel;
+					bMi1, bFa1, bSo1, bLa1, bti1, bDo2, btSave, btClear;
+	
+	JPanel panel1, panel2;
 	
 	public NewPartition() {
 		setBackground(Color.ORANGE);
+		
+		panel1 = new JPanel();
+			tfName = new JTextField();
+			tfNotes = new JTextField();
+		
+	        bDo2 = new JButton();
+	        bLa1 = new JButton();
+	        bti1 = new JButton();
+	        bFa1 = new JButton();
+	        bSo1 = new JButton();
+	        bRe1 = new JButton();
+	        bMi1 = new JButton();
+	        bRe = new JButton();
+	        bDo1 = new JButton();
+	        bSo = new JButton();
+	        bFa = new JButton();
+	        bMi = new JButton();
+	        bDo = new JButton();
+	        bti = new JButton();
+	        bLa = new JButton();
+	        
+	        btClear = new JButton();
+	        btSave = new JButton();
+	        
+        panel2 = new JPanel();
 
-		tfNotes = new JTextField();
-		tfNotes.setPreferredSize(new Dimension(50, 24));
-//		tfNotes.setColumns(50);
-		
-		
-		add(tfNotes);
-		
-		bDo = new JButton("Do");
-		bDo.setPreferredSize(new Dimension(100, 100));
-		
-		bRe = new JButton("Re");
-		bRe.setPreferredSize(new Dimension(100, 100));
-		
-		bMi = new JButton("Mi");
-		bMi.setPreferredSize(new Dimension(100, 100));
-		
-		bFa = new JButton("Fa");
-		bFa.setPreferredSize(new Dimension(100, 100));
-		
-		bSo = new JButton("So");
-		bSo.setPreferredSize(new Dimension(100, 100));
-		
-		bLa = new JButton("La");
-		bLa.setPreferredSize(new Dimension(100, 100));
-		
-		bti = new JButton("ti");
-		bti.setPreferredSize(new Dimension(100, 100));
-		
-		bDo1 = new JButton("Do1");
-		bDo1.setPreferredSize(new Dimension(100, 100));
-		
-		bRe1 = new JButton("Re1");
-		bRe1.setPreferredSize(new Dimension(100, 100));
-		
-		bMi1 = new JButton("Mi1");
-		bMi1.setPreferredSize(new Dimension(100, 100));
-		
-		bFa1 = new JButton("Fa1");
-		bFa1.setPreferredSize(new Dimension(100, 100));
-		
-		bSo1 = new JButton("So1");
-		bSo1.setPreferredSize(new Dimension(100, 100));
-		
-		bLa1 = new JButton("La1");
-		bLa1.setPreferredSize(new Dimension(100, 100));
-		
-		bti1 = new JButton("ti1");
-		bti1.setPreferredSize(new Dimension(100, 100));
-		
-		bDo2 = new JButton("Do2");
-		bDo2.setPreferredSize(new Dimension(100, 100));
-		
-		
-		setLayout(new GridBagLayout());
-		GridBagConstraints gbc = new GridBagConstraints();
-		
-		
-		gbc.gridx = gbc.gridy = 0;
-		
-		//La taille en hauteur et en largeur
-	    gbc.gridheight = 1;
-	    gbc.gridwidth = 1;
-	    add(bDo, gbc);
-	    
-	    gbc.gridx = 1;
-	    add(bRe, gbc);
-	    
-	    gbc.gridx = 2;
-	    add(bMi, gbc);
-	    
-	    gbc.gridx = 3;
-	    add(bFa, gbc);
-	    
-	    gbc.gridwidth = GridBagConstraints.REMAINDER;
-	    gbc.gridx = 4;
-	    add(bSo, gbc);
-	    
-	    gbc.gridx = 0;
-	    gbc.gridy = 1;
-	    gbc.gridwidth = 1;
-	    gbc.gridheight = 1;
-	    add(bLa, gbc);
-	    
-	    gbc.gridx = 1;
-	    add(bti, gbc);
-	    
-	    gbc.gridx = 2;
-	    add(bDo1, gbc);
-	    
-	    gbc.gridx = 3;
-	    add(bRe1, gbc);
-	    
-	    gbc.gridwidth = GridBagConstraints.REMAINDER;
-	    gbc.gridx = 4;
-	    add(bMi1, gbc);
-	    
-	    gbc.gridx = 0;
-	    gbc.gridy = 2;
-	    gbc.gridwidth = 1;
-	    gbc.gridheight = 1;
-	    add(bFa1, gbc);
-	    
-	    gbc.gridx = 1;
-	    add(bSo1, gbc);
-	    
-	    gbc.gridx = 2;
-	    add(bLa1, gbc);
-	    
-	    gbc.gridx = 3;
-	    add(bti1, gbc);
-	    
-	    gbc.gridwidth = GridBagConstraints.REMAINDER;
-	    gbc.gridx = 4;
-	    add(bDo2, gbc);
+        //-----------------------------------------------------
+        panel1.setBackground(new java.awt.Color(247, 20, 20));
 
+        bDo2.setText("bDo2");
+        bDo2.addActionListener(new ChiffreListener());
+
+        btSave.setText("btSave");
+
+        bLa1.setText("bLa1");
+        bLa1.addActionListener(new ChiffreListener());
+
+        bti1.setText("bti1");
+        bti1.addActionListener(new ChiffreListener());
+
+        bFa1.setText("bFa1");
+        bFa1.addActionListener(new ChiffreListener());
+
+        bSo1.setText("bSo1");
+        bSo1.addActionListener(new ChiffreListener());
+
+        bRe1.setText("bRe1");
+        bRe1.addActionListener(new ChiffreListener());
+
+        bMi1.setText("bMi1");
+        bMi1.addActionListener(new ChiffreListener());
+
+        bRe.setText("bRe");
+        bRe.addActionListener(new ChiffreListener());
+        
+        btClear.setText("btClear");
+        btClear.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                tfNotes.setText(null);
+            }
+        });
+
+        bDo1.setText("bDo1");
+        bDo1.addActionListener(new ChiffreListener());
+        
+        bSo.setText("bSo");
+        bSo.addActionListener(new ChiffreListener());
+        
+        bFa.setText("bFa");
+        bFa.addActionListener(new ChiffreListener());
+        
+        bMi.setText("bMi");
+        bMi.addActionListener(new ChiffreListener());
+        
+        bDo.setText("bDo");
+        bDo.addActionListener(new ChiffreListener());
+        
+        bti.setText("bti");
+        bti.addActionListener(new ChiffreListener());
+        
+        bLa.setText("bLa");
+        bLa.addActionListener(new ChiffreListener());
+        
+        javax.swing.GroupLayout panel1Layout = new javax.swing.GroupLayout(panel1);
+        panel1.setLayout(panel1Layout);
+        panel1Layout.setHorizontalGroup(
+            panel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(panel1Layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(panel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(panel1Layout.createSequentialGroup()
+                        .addComponent(bLa, javax.swing.GroupLayout.PREFERRED_SIZE, 75, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(bti, javax.swing.GroupLayout.PREFERRED_SIZE, 75, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(bDo1, javax.swing.GroupLayout.PREFERRED_SIZE, 75, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(bRe1, javax.swing.GroupLayout.PREFERRED_SIZE, 75, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(bMi1, javax.swing.GroupLayout.PREFERRED_SIZE, 75, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(panel1Layout.createSequentialGroup()
+                        .addComponent(bFa1, javax.swing.GroupLayout.PREFERRED_SIZE, 75, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(bSo1, javax.swing.GroupLayout.PREFERRED_SIZE, 75, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addGroup(panel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, panel1Layout.createSequentialGroup()
+                                .addComponent(btClear)
+                                .addGap(18, 18, 18)
+                                .addComponent(btSave))
+                            .addGroup(panel1Layout.createSequentialGroup()
+                                .addComponent(bLa1, javax.swing.GroupLayout.PREFERRED_SIZE, 75, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(bti1, javax.swing.GroupLayout.PREFERRED_SIZE, 75, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(bDo2, javax.swing.GroupLayout.PREFERRED_SIZE, 75, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                    .addGroup(panel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                        .addComponent(tfNotes, javax.swing.GroupLayout.Alignment.LEADING)
+                        .addGroup(panel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(panel1Layout.createSequentialGroup()
+                                .addGap(155, 155, 155)
+                                .addComponent(tfName, javax.swing.GroupLayout.PREFERRED_SIZE, 122, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            .addGroup(panel1Layout.createSequentialGroup()
+                                .addComponent(bRe, javax.swing.GroupLayout.PREFERRED_SIZE, 75, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(bDo, javax.swing.GroupLayout.PREFERRED_SIZE, 75, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(bMi, javax.swing.GroupLayout.PREFERRED_SIZE, 75, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(bFa, javax.swing.GroupLayout.PREFERRED_SIZE, 75, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(bSo, javax.swing.GroupLayout.PREFERRED_SIZE, 75, javax.swing.GroupLayout.PREFERRED_SIZE)))))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+        panel1Layout.setVerticalGroup(
+            panel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(panel1Layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(tfName, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(tfNotes, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(panel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(bRe, javax.swing.GroupLayout.PREFERRED_SIZE, 75, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(bDo, javax.swing.GroupLayout.PREFERRED_SIZE, 75, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(bMi, javax.swing.GroupLayout.PREFERRED_SIZE, 75, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(bFa, javax.swing.GroupLayout.PREFERRED_SIZE, 75, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(bSo, javax.swing.GroupLayout.PREFERRED_SIZE, 75, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(26, 26, 26)
+                .addGroup(panel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(bLa, javax.swing.GroupLayout.PREFERRED_SIZE, 75, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(bti, javax.swing.GroupLayout.PREFERRED_SIZE, 75, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(bDo1, javax.swing.GroupLayout.PREFERRED_SIZE, 75, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(bRe1, javax.swing.GroupLayout.PREFERRED_SIZE, 75, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(bMi1, javax.swing.GroupLayout.PREFERRED_SIZE, 75, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(18, 18, 18)
+                .addGroup(panel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(bFa1, javax.swing.GroupLayout.PREFERRED_SIZE, 75, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(bSo1, javax.swing.GroupLayout.PREFERRED_SIZE, 75, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(bLa1, javax.swing.GroupLayout.PREFERRED_SIZE, 75, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(bti1, javax.swing.GroupLayout.PREFERRED_SIZE, 75, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(bDo2, javax.swing.GroupLayout.PREFERRED_SIZE, 75, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(panel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(btClear)
+                    .addComponent(btSave))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+
+        panel2.setBackground(new java.awt.Color(13, 112, 241));
+
+        javax.swing.GroupLayout panel2Layout = new javax.swing.GroupLayout(panel2);
+        panel2.setLayout(panel2Layout);
+        panel2Layout.setHorizontalGroup(
+            panel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 622, Short.MAX_VALUE)
+        );
+        panel2Layout.setVerticalGroup(
+            panel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 0, Short.MAX_VALUE)
+        );
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
+        this.setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addComponent(panel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(panel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addComponent(panel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 469, Short.MAX_VALUE))
+            .addComponent(panel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
 	}
 
+	class ChiffreListener implements ActionListener {
+	    public void actionPerformed(ActionEvent e){
+	    	String str = ((JButton)e.getSource()).getText();
+	    	if(!(tfNotes.getText().length() == 0))
+	    		str = tfNotes.getText() + " - "+ str;
+	    	tfNotes.setText(str);
+	   }
+	 }
+	
+	public void actionPerformed(ActionEvent click)  {
+
+		Object source = click.getSource();
+    
+		JOptionPane.showMessageDialog(tfName, "tetertert");
+    }
+	
 	private void setContentPane(JPanel pan) {
 		// TODO Auto-generated method stub
 		
