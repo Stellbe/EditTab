66cd0ddccbddfcbf7b6c0c1af06575486e31b432
stellbe <remydagorne@hotmail.fr>
2014-06-23 17:10:37 +0200

    - Début d'un migLayout

--- src/com/editab/views/MusicScorePanel.java
+++ src/com/editab/views/MusicScorePanel.java
@@ -7,7 +7,11 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+import net.miginfocom.swing.MigLayout;
 
 import com.editab.models.Score;
 
@@ -19,12 +23,15 @@ public class MusicScorePanel extends JPanel {
 	private static final short NOTE_NUMBER = 15; 
 	
 	private static final String [] note_label = new String [] {
-		"Do","Re","Mi","Fa","Sol","Je","Connais","Pas","Les","Autres","","","","",""
+		"Do", "Re", "Mi", "Fa", "So", "La", "Ti", "Do1", "Re1", "Mi1", "Fa1",
+		"So1", "La1", "Ti1", "Do2"
 	};
 	
 	/**
 	 * GUI Elements
 	 */
+	
+	private JTextField nameScore = new JTextField(30);
 	private JButton [] buttons = new JButton[NOTE_NUMBER];
 	private JButton bClear = new JButton("Clear");
 	private JButton bSave = new JButton("Save");
@@ -49,10 +56,15 @@ public class MusicScorePanel extends JPanel {
 	
 	public void initPanel () {
 		
-		leftPanel.setLayout(new GridLayout(8,8));
+		leftPanel.setLayout(new MigLayout("fillx, wrap 5, gap 10px 30px"));
+//		leftPanel.setLayout(new GridLayout(3, 5));
 		
+		leftPanel.add(new JLabel("Nom de la partition :"));
+		leftPanel.add(nameScore, "wrap");
 		leftPanel.setBackground(Color.GRAY);
 		rightPanel.setBackground(Color.GRAY);
+		
+		
 		
 		
 		for(int i = 0; i < buttons.length; i++) {
@@ -61,7 +73,7 @@ public class MusicScorePanel extends JPanel {
 			
 			buttons[i].addActionListener(noteListener);
 			
-			leftPanel.add(buttons[i]);
+			leftPanel.add(buttons[i], "width 70:70:70, height 120:120:120, growx");
 		}
 		
 		add(leftPanel);
